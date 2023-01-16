54453a9839a9d60d610aab86cb275cad65da2d56
shlapique <gordsdima@gmail.com>
2023-01-16 22:33:03 +0300

    init

---
+++ lectures-ms/build.R
@@ -0,0 +1,11 @@
+#!/usr/bin/env Rscript
+require("rmarkdown");
+
+render(
+    "main.rmd",
+    output_format = "pdf_document",
+    output_file = "report.pdf",
+    output_dir = "output/",
+    run_pandoc = TRUE,
+    encoding = "UTF-8"
+);
