d99519417323d5fdb02392976b3a339e5133dc8f
shlapique <gordsdima@gmail.com>
2023-01-30 02:55:20 +0300

    almost done with first graph, todo: optimize plot on paper

--- lectures-ms/chunks.R
+++ lectures-ms/chunks.R
@@ -8,7 +8,7 @@ check_device <- function()
 plotter <- function(data)
 {
     print("OUT OF PLOTTER")
-    dash_data <- data.frame()
+    dash_data <- data.frame() # vertical dashed lines
     dash_data_tmp <- dash_data
     for(i in 1:(length(data[, 1])-1))
     {
@@ -17,12 +17,29 @@ plotter <- function(data)
         {
             dash_data_tmp <- dash_data
             dash_data <- rbind(dash_data_tmp, data.frame("x"=c(data[i, 1], data[i, 1]), "f"=c(data[i, 2], data[i+1, 2])))
-            # ggplot(data.frame("x"=c(data[i, 1], data[i, 1]), "f"=c(data[i, 2], data[i+1, 2])), mapping=aes(x=x, y=f)) + geom_line(linetype="dashed")
         }
     }
     print("DAASH DATA IS")
     print(dash_data)
-    ggplot(dash_data, aes(x=x, y=f)) + geom_line(linetype="dashed")
+    p <- ggplot(data.frame("x"=c(data[1, 1], dash_data[1, 1]), "f"=c(data[1, 2], dash_data[1, 2])), mapping=aes(x=x, y=f)) + geom_line() + geom_point(data.frame("x"=c(dash_data[1, 1]), "f"=c(dash_data[1, 2])), mapping=aes(x=x, y=f), shape=21, size=3, fill="white")
+    j <- 1
+    m <- length(dash_data[, 1]) - 1
+    while(j <= length(dash_data[, 1]))
+    {
+        data_tmp <- data.frame("x"=c(dash_data[j, 1], dash_data[j+1, 1]), "f"=c(dash_data[j, 2], dash_data[j+1, 2]))
+        p <- p + geom_line(data_tmp, mapping=aes(x=x, y=f), linetype="dashed")
+        if(m-1 != 0)
+        {
+            data_tmp <- data.frame("x"=c(dash_data[m-1, 1], dash_data[m, 1]), "f"=c(dash_data[m-1, 2], dash_data[m, 2]))
+            p <- p + geom_line(data_tmp, mapping=aes(x=x, y=f)) + geom_point(data.frame("x"=c(dash_data[m, 1]), "f"=c(dash_data[m, 2])), mapping=aes(x=x, y=f), shape=21, size=3, fill="white")
+            m <- m - 2
+        }
+        j <- j + 2
+    }
+    p <- p + geom_line(data.frame("x"=c(dash_data[length(dash_data[,1]), 1], data[length(data[,1]), 1]), "f"=c(dash_data[length(dash_data[,1]), 2], data[length(data[,1]), 2])), mapping=aes(x=x, y=f))
+    p <- p + scale_x_continuous(breaks=c(dash_data[1, 1], dash_data[3, 1], 0, dash_data[length(dash_data[, 1])-2, 1], dash_data[length(dash_data[, 1]), 1]), labels=c("X^(1)", "X^(2)", "0", "X^(n-1)", "X^(n)"))
+    p <- p + scale_y_continuous(breaks=c(1/length(data[, 1]), 2/length(data[, 1]), dash_data[length(dash_data[, 1])-1, 2], 1), labels=c("1/n", "2/n", "1 - 1/n", "1"))
+    p
 }
 
 # if x >= X^{(1)}
@@ -63,4 +80,6 @@ x <- seq(-5, 5, 1)
 y <- rnorm(n, 0, 1)
 data <- por_stat(n, x, sort(y))
 data
+X11()
 plotter(data)
+check_device()
