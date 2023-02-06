083a10049c47c2a13fe6736dc6fbf1f7054081e8
shlapique <gordsdima@gmail.com>
2023-02-06 19:17:54 +0300

    fixes in hist()

--- lectures-ms/hist.R
+++ lectures-ms/hist.R
@@ -8,7 +8,8 @@ check_device <- function()
 
 histo <- function(data)
 {
-    p <- ggplot(data.frame("x"=c(-3, data[1, 1]), "f"=c(0, 0)), mapping=aes(x=x, y=f)) + geom_line()
+    # p <- ggplot(data.frame("x"=c(-3, data[1, 1]), "f"=c(0, 0)), mapping=aes(x=x, y=f)) + geom_line()
+    p <- ggplot() + geom_vline(xintercept=0) + geom_hline(yintercept=0)
     for(i in 1:(length(data[, 1])-1))
     {
         p <- p + geom_line(data.frame("x"=c(data[i, 1], data[i+1, 1]), "f"=c(data[i, 2], data[i, 2])), mapping=aes(x=x, y=f)) 
@@ -16,15 +17,21 @@ histo <- function(data)
         print(i)
     }
     p <- p + geom_line(data.frame("x"=c(data[1, 1], data[1, 1]), "f"=c(data[1, 2], 0)), mapping=aes(x=x, y=f), linetype="dashed", colour="grey")
-    for(i in 1:length(data[, 1]))
+    for(i in 2:(length(data[, 1])-1))
     {
-        p <- p + geom_line(data.frame("x"=c(data[i, 1], data[i, 1]), "f"=c(data[i, 2], 0)), mapping=aes(x=x, y=f), linetype="dashed", colour="grey")
+        if(data[i, 2] > data[i-1, 2])
+        {
+            p <- p + geom_line(data.frame("x"=c(data[i, 1], data[i, 1]), "f"=c(data[i, 2], 0)), mapping=aes(x=x, y=f), linetype="dashed", colour="grey")
+        } else # if current point value is less then previous
+        {
+            p <- p + geom_line(data.frame("x"=c(data[i, 1], data[i, 1]), "f"=c(data[i-1, 2], 0)), mapping=aes(x=x, y=f), linetype="dashed", colour="grey")
+        }
     }
-    p <- p + geom_line(data.frame("x"=c(data[length(data[, 1]), 1], data[length(data[, 1]), 1]), "f"=c(data[length(data[, 1]), 2], 0)), mapping=aes(x=x, y=f), linetype="dashed", colour="grey")
+    p <- p + geom_line(data.frame("x"=c(data[length(data[, 1]), 1], data[length(data[, 1]), 1]), "f"=c(data[length(data[, 1])-1, 2], 0)), mapping=aes(x=x, y=f), linetype="dashed", colour="grey")
     p
 }
 
-data <- data.frame("x"=c(seq(-2, 4, 1)), "f"=0)
+data <- data.frame("x"=c(seq(-2, 3, 1)), "f"=0)
 for(i in 1:length(data[, 1]))
 {
     data[i, 2] <- abs(runif(1, min=0, max=1))
