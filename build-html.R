20cee2bbc6bb1cbe16c363d43fcd53dfcae3c612
shlapique <gordsdima@gmail.com>
2023-11-19 00:17:03 +0300

    renamed html to index)

--- build-html.R
+++ build-html.R
@@ -4,7 +4,7 @@ require("rmarkdown");
 render(
     "main.rmd",
     output_format = "html_document",
-    output_file = "lectures-ms.html",
+    output_file = "index.html",
     output_dir = "docs/",
     run_pandoc = TRUE,
     encoding = "UTF-8"
